41dcd573cc03c1339e964c47aac77e2dbcf0568c
Evening <evening@Admins-iMac.local>
2022-11-28 17:31:52 +0800

    Javascript

---
+++ javascript/js_basic/main.js
@@ -0,0 +1,66 @@
+// Comments can make code readable
+
+// Javascript lesson begin
+
+/* Comments can
+ make code readable */
+
+// Variable.js нэртэй файл үүсгэх, number, string, boolean, undefined, null зэрэг утгуудыг өгөх
+let age = 22;
+let ner = "Tushig";
+let student = true;
+let leap ;
+let nest = null;
+
+console.log(age);
+console.log(ner);
+console.log(student);
+console.log(leap);
+console.log(nest);
+
+// Datatypes.js нэртэй файл үүсгэх, typeof ашиглан өөр, өөр, төрлийн утгуудыг шалгах, number, string, boolean, undefined,  null гэх мэт 
+console.log(typeof(age));
+console.log(typeof(ner));
+console.log(typeof(student));
+console.log(typeof(leap));
+console.log(typeof(nest));
+
+// Утга оноогоогүй хувьсагч зарлах
+let emptyvar = "";
+console.log(emptyvar);
+
+// Утга оноосон хувьсагч зарлах 
+let huvisagch = "abc123";
+console.log(huvisagch);
+
+// Олон мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах
+let fname = "Tushig";
+let lname = "Regzen";
+let marital_status = "single";
+let country = "MGL";
+let nas = 22;
+
+console.log(fname);
+console.log(lname);
+console.log(marital_status);
+console.log(country);
+console.log(nas);
+
+// Нэг мөрөнд First name, last name, marital status, country, age гэсэн нэртэй хувьсагч зарлах 
+console.log(fname + " " + lname + " " + marital_status + " " + country + " " + nas);
+
+// MyAge, yourAge нэртэй хувьсагч зарлан эхний утгыг оноох
+let my_age = 22;
+let your_age = 33;
+console.log(my_age);
+console.log(your_age);
+
+// Гурвалжингийн периметрийг олох 
+let x = 4;
+let y = 4;
+let z = 4;
+console.log("x =" + x);
+console.log("y =" + y);
+console.log("z =" + z);
+console.log("p =", x + y + z);
+
