313190a9f550398bfda7d75b0d8947b04ad68011
Evening <evening@Admins-iMac.local>
2022-12-05 17:36:28 +0800

    12.5

---
+++ javascript/js_array/main.js
@@ -0,0 +1,74 @@
+// ARRAY OF NUMBERS
+
+// 1. arrayOfNumbers гэсэн variable зарлаад дараах тоонуудыг оруул. 43, 56, 23, 89, 88, 90, 99, 652, 15, 290, 11
+
+// let arrayOfNumbers = [43, 56, 23, 89, 88, 90, 99, 652, 15, 290, 11];
+
+// 2. Array доторх тоонуудын нийлбэрийг ол.
+
+// let sum = 0;
+
+// for(let i=0; i < arrayOfNumbers.length; i++){
+//     sum += arrayOfNumbers[i]
+// }
+// console.log(`Sum of all numbers: ${sum}`);
+
+// 3. Хамгийн их тоог ол.
+
+// let max = 0;
+
+// for(let i = 0; i < arrayOfNumbers.length; i++){
+//     if(max < arrayOfNumbers[i]){
+//         max = arrayOfNumbers[i];
+//     }
+// }
+// console.log(`hamgiin ih utga n: ${max}`);
+
+
+// 4. Хамгийн бага тоог ол.
+
+// let min = arrayOfNumbers[1];
+
+// for(let i = 0; i < arrayOfNumbers.length; i++){
+//     if(min > arrayOfNumbers[i]){
+//         min =arrayOfNumbers[i];
+//     }
+// }
+// console.log(`hamgiin baga utga n: ${min}`);
+
+
+// 5. Array - ийн эхэнд дурын 1 тоог нэм.
+
+//  arrayOfNumbers.unshift(77);
+//  console.log(arrayOfNumbers);
+
+ // 6. Array - ийн төгсгөлд дурын 1 тоог нэм.
+
+//  arrayOfNumbers.push(88);
+//  console.log(arrayOfNumbers);
+
+
+// SIMPLE ARRAY - 1
+
+
+// Array - ын хувьсагчийг itCompanies гэж зарлаж, Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon зэрэг анхны утгыг оруулна уу. 
+
+let itCompanies = [`Facebook`, `Google`, `Microsoft`, `Apple`, `IBM`, `Oracle`, `Amazon`];
+
+//console.log() ашиглан array хэвлэх
+console.log(itCompanies);
+
+//Array дахь компаниудын length хэвлэх
+console.log(itCompanies.length);
+
+//Эхний компани, дунд болон сүүлчийн компанийг хэвлэх
+console.log(itCompanies[0], itCompanies[3], itCompanies[6]);
+
+//Компани бүрийг хэвлэх
+console.log(`${itCompanies}`);
+
+//Компанийн нэр тус бүрийг нэг нэгээр нь том үсгээр сольж хэвлэж гарга
+console.log(itCompanies[0].toLowerCase());
+
+//Array - г өгүүлбэр болгон хэвлэ: Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon зэрэг мэдээллийн технологийн томоохон компаниуд.
+console.log(`${itCompanies} зэрэг мэдээллийн технологийн томоохон компаниуд.`);
--- javascript/js_in_class/app.js
+++ javascript/js_in_class/app.js
@@ -42,8 +42,41 @@
 
 // js loop
 
-let count = 1;
-while (count <= 10) {
-console.log(count);
-count = count + 1;
-}
+// let count = 1;
+// while (count <= 10) {
+// console.log(count);
+// count = count + 1;
+// }
+
+// js array
+
+// let studentNames = ['boldo', 'dorjo', 'tsetsgee','dulmaa']
+// let i = 0;
+// while (i < studentNames.length) {
+
+//     if (studentNames[i][0] === 'd') {
+//     console.log(studentNames[i]);
+//     }
+//     i++;
+
+// }
+
+// let studentAges = [10, 20, 15, 19, 18,19 ,19];
+// let i = 0;
+// let sum = 0;
+// while (i < studentAges.length) {
+// sum+=studentAges[i]
+// i++;
+// }
+// let avarage = sum/studentAges.length
+// console.log(avarage);
+
+// let userBalance = [1000, 2000, 2500, 4000000, 13054,323425];
+// let i = 0;
+// while (i < userBalance.length) {
+//     userBalance[i] *= 1.15;
+//     i++;
+//     }
+// console.log(userBalance);
+
+
