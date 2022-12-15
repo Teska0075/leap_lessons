90274f13578491c8c3030d84563a2b7f1ed1eb3d
Evening <evening@Admins-iMac.local>
2022-12-15 18:15:44 +0800

    todolist DONE XD

--- javascript/js_dom_git/main.js
+++ javascript/js_dom_git/main.js
@@ -5,29 +5,35 @@ console.log(todayDate);
 
 document.getElementById("date_span").innerHTML = todayDate
 
+// tasks left
+
+
 // task input
-// let listsToDo = document.getElementById('todolist');
+
 
 const addBtn = document.getElementsByTagName("button")[0];
 const inputText = document.getElementById("task_top");
 const taskList = document.getElementById("bottom_sec");
-
+let count = 0;
 const cardItem = (text) => {
     const item = `
         <div id='todolist' class="d-flex form-control rounded-2 mt-2">
             
             <input class="task_bot form-control border-0 " type="text" name="task" id="task_bot" placeholder="test" value="${text}" readonly>
+            <div class="actions d-flex">
+                <button onclick="edit(this)" class="btn"><i class="fa-solid fa-pen"></i></button>
 
-            <button onclick="edit(this)" class="btn"><i class="fa-solid fa-pen"></i></button>
+                <button onclick="checked(this)" class="btn"><i class="fa-solid fa-check"></i></button>
 
-            <button onclick="checked()" class="btn"><i class="fa-solid fa-check"></i></button>
-
-            <button onclick="ustgah(this)" class="btn"><i class="fa-solid fa-trash"></i></button>
+                <button onclick="ustgah(this)" class="btn"><i class="fa-solid fa-trash"></i></button>
+            </div>
 
         </div>`;
 
     return item;
 }
+
+
 
 const addTask = () => {
     let text = inputText.value;
@@ -36,6 +42,13 @@ const addTask = () => {
     }else{
         taskList.innerHTML += cardItem(text);
     }
+
+    task = document.getElementById("tasks_left");
+    console.log(task);
+    count ++;
+    task.innerHTML = `${count} tasks left`;
+
+
 }
 
 addBtn.addEventListener("click", addTask);
@@ -55,96 +68,42 @@ const delBtn = document.getElementsByTagName("button")[3];
 let ustgah = (e) => {
     console.log(e.parentNode.parentNode);
 
-    const parent = e.parentNode.parentNode;
-    const child = e.parentNode;
+    const parent = e.parentNode.parentNode.parentNode;
+    const child = e.parentNode.parentNode;
 
     parent.removeChild(child);
+
+    task = document.getElementById("tasks_left");
+    console.log(task);
+    count --;
+    task.innerHTML = `${count} tasks left`;
 }
 
-let checked = () => {
-    document.querySelector("#task_bot").style.textDecoration = "line-through";
+let checked = (e) => {
+    let input = e.parentNode.parentNode.children[0]; // selecting input
+
+    input.style.textDecoration = "line-through";
 }
 
 
 let edit = (e) =>{
-    var editValue = prompt('edit the select item', e.parentNode.value);
-    e.parentNode.value = editValue;
-    console.log(editValue);
-    console.log(e.parentNode);
+    let input = e.parentNode.parentNode.children[0]; // selecting input
+    console.log(input.value);
+    input.setAttribute("value", input.value)
     
-    
-    
-    // let parent = e.parentNode.parentNode;
-    // let edit = parent.querySelector("input");
-    // console.log(edit);
-    // edit.removeAttribute("readonly");
+    // let icon1 = e.parentNode.children[0]; // selecting button tag
+    // console.log(icon1);
 
+    let icon2 = e.children[0]; // selecting icon tag inside of button
+    console.log(icon2);
 
-
-    // let input = document.querySelector(".task_bot");
-    // const inputValue = input.value
-    // console.log(inputValue);
-    // input.removeAttribute("readonly");
-    
-    // let removeBtn = document.getElementsByTagName("button")[1];
-    // console.log(removeBtn);
-    // editBtn.remove();
-    
-    // document.getElementsByTagName("button")[1].remove();
-
-    // let icon = document.getElementsByTagName("i")[0];
-    // icon.classList.toggle("fa-floppy-disk");
+    if(icon2.classList.contains("fa-pen")){
+        icon2.classList.remove("fa-pen");
+        icon2.classList.add("fa-floppy-disk");
+        input.removeAttribute("readonly");
+    }else{
+        icon2.classList.remove("fa-floppy-disk");
+        icon2.classList.add("fa-pen");
+        input.getAttribute("readonly");
+    }
 }
-
-
-
-
-    // input.setAttribute("readonly")
-
-    // x.classList.toggle("fa-thumbs-down");
-
-const saveBtn = `<button onclick="save()" class="btn"><i class="fa-solid fa-floppy-disk"></i></button>`;
-// let save = (e) =>{
-//     let input = document.querySelector("#task_bot");
-//     input.append(saveBtn);
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function transfer(){
-//     let toDoList = document.querySelector("#task_top").value;
-//     let taskDiv = document.createElement("div");
-    // let items = `<div id='todolist' class="d-flex form-control rounded-0 mt-2">
-    // <input class="form-control border-0 " type="text" name="task" id="task_bot" placeholder="test" value="" readonly>
-    // <button class="btn"><i class="fa-solid fa-pen"></i></button>
-    // <button class="btn"><i class="fa-solid fa-check"></i></button>
-    // <button class="btn"><i class="fa-solid fa-trash"></i></button>
-    // </div>`;
-
-//     taskDiv.innerHTML=items;
-//     listsToDo.appendChild(taskDiv);
-//     taskDiv.getElementsByTagName("input")[0].value = toDoList;
-    
-    
-    
-// }
-
-// addBtn.addEventListener("click",transfer);
-
-
-// let taskInput = document.getElementById("task_top").innerText;
-// let taskBot = document.getElementById("task_bottom");
-// taskBot.value = taskInput;
