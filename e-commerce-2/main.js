955cdfb5c9d2ded05101ae6ea09cc9e652b0d442
Teska0075 <tushigregzen@gmail.com>
2022-12-27 12:42:04 +0800

    ecomm sidebar

--- e-commerce-2/main.js
+++ e-commerce-2/main.js
@@ -8,27 +8,24 @@ let allProducts = [];
 const getProducts = async () => {
 
     const response = await fetch('https://dummyjson.com/products');
-
     const data = await response.json();    
-
     allProducts = data.products;
-
     displayProduct();
-    
+
 }
 getProducts();
 
 const displayProduct = () => {
     allProducts.forEach((product) => {
-        const item = `
-        <div class="card" style="width: 18rem;">
-        <img src="${product.thumbnail}" class="card-img-top" style = "width: 100%; height: 200px;alt="...">
-        <div class="card-body">
+        const card = `
+        <div class="card shadow p-2 bg-body rounded border-0" style="width: 18rem;">
+        <img src="${product.thumbnail}" class="card-img-top" style = "width: 100%; height: 200px;" alt="...">
+        <div class="card-body d-flex flex-column">
             <h3 class="card-title text-center fw-bolder fs-3">${product.title}</h3>
-            <p class="text-end text-success fw-bold fs-5">${product.discountPercentage} % OFF</p>
+            <p class="text-success fs-6 mt-3" style="margin-left: auto; height: 27px; text-align: end; background-color: lightgreen;">${product.discountPercentage}% OFF</p>
             <h4>$${product.price}.00</h4>
             <p class="card-text fs-6 w-100 text-truncate">${product.description}.</p>
-            <div class="d-flex justify-content-between">
+            <div class="d-flex justify-content-between mt-3 mb-0">
                 <div>
                     <span><i class="bi-solid bi-star-fill text-warning"></i></span>
                     <span><i class="bi-solid bi-star-fill text-warning"></i></span>
@@ -38,11 +35,10 @@ const displayProduct = () => {
                     <span>${product.rating}</span>   
                 </div>
                 <div>
-                    <a href="#" class="btn btn-white text-primary border-primary">
-                    <i class="bi bi-cart-plus"></i>
-                        
-                        Sagslah
-                    </a>
+                    <button href="#" class="btn btn-white fw-bold text-primary border-primary">
+                    <i class="bi bi-cart"></i>
+                        Cart
+                    </button>
                 </div>
                
                 
@@ -51,6 +47,6 @@ const displayProduct = () => {
         </div>
     </div>
     `;
-    productList.innerHTML += item;
+    productList.innerHTML += card;
     });
 };
