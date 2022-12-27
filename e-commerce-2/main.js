459ed610809e0b1cef39c3bc7287eb9806ed83ac
Evening <evening@Admins-iMac.local>
2022-12-27 18:55:45 +0800

    almost done

--- e-commerce-2/main.js
+++ e-commerce-2/main.js
@@ -1,9 +1,12 @@
 console.log('Ecommerce');
 
 // All variables
-
 const productList = document.querySelector(".productList");
+const cartList = document.querySelector(".cartList");
+const cartCount = document.querySelector(".cartCount")
+// const productsEvent = document.querySelector(".productsEvent");
 let allProducts = [];
+let cartProducts = [];
 
 const getProducts = async () => {
 
@@ -11,12 +14,52 @@ const getProducts = async () => {
     const data = await response.json();    
     allProducts = data.products;
     displayProduct();
-
+    displayCart();
 }
 getProducts();
 
+const addCart = (idx) => {
+    cartProducts.push(allProducts[idx]);
+    cartCount.innerText = cartProducts.length;
+    displayCart();
+  };
+
+const displayCart = () => {
+    cartList.innerHTML = '';
+    for (product of cartProducts){
+        const cartItem = `
+                <div class="card mt-1 p-4">
+                    <div class="row  d-flex align-items-center">
+                        <div class="d-flex flex-column col-1 align-items-center">
+                            <button class="btn border border-1">+</button>
+                            <span class="my-2">2</span>
+                            <button class="btn border border-1">-</button>
+                        </div>
+                        <div class="col-4 ">
+                            <img class="card-img w-100 h-80" src="${product.thumbnail}"  alt="">
+                        </div>
+                        
+                        <div class="col-4 ">
+                            <h3 class="card-title fs-5">${product.title}</h3>
+                            <h5 class="fs-2 text-primary">$${product.price}.00</h4>
+                            <h3 class="fs-6 fw-light  text-muted">2 X</h3>
+                        </div>
+                        
+                        <div class="col-3 d-flex align-items-center border-start border-2">
+                            <h5 class="ms-3">$240.00</h4>
+                            
+                        </div>
+                    </div>
+                </div>
+        `;
+        cartList.innerHTML += cartItem;
+    };
+};
+
+
+
 const displayProduct = () => {
-    allProducts.forEach((product) => {
+    allProducts.forEach((product,idx) => {
         const card = `
         <div class="card shadow p-2 bg-body rounded border-0" style="width: 18rem;">
         <img src="${product.thumbnail}" class="card-img-top" style = "width: 100%; height: 200px;" alt="...">
@@ -35,7 +78,7 @@ const displayProduct = () => {
                     <span>${product.rating}</span>   
                 </div>
                 <div>
-                    <button href="#" class="btn btn-white fw-bold text-primary border-primary">
+                    <button href="#" onclick="addCart(${idx})" class="btn btn-white fw-bold text-primary border-primary">
                     <i class="bi bi-cart"></i>
                         Cart
                     </button>
@@ -50,3 +93,4 @@ const displayProduct = () => {
     productList.innerHTML += card;
     });
 };
+
